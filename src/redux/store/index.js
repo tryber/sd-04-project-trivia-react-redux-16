3da162a8ee947242998ac6beb4308d3231cb996f
Lucas Ribas <lucas@mundoribas.com.br>
2020-07-17 15:21:57 -0300

    uPDATING MODIFICATIONS

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer,
-  compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(rootReducer);
 
 export default store;
