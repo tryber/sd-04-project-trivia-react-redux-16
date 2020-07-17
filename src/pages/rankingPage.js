b77b3e5d80f934cc58b2cbb95d0896beaa626078
Lucas Ribas <lucas@mundoribas.com.br>
2020-07-17 16:56:21 -0300

    Esqueleto da rankingPage

--- src/pages/rankingPage.js
+++ src/pages/rankingPage.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class RankingPage extends Component {
+
   render() {
+    // const ranking = JSON.parse(localStorage.getIten('ranking'))
+    const ranking = [
+      {name: "primeirapessoa", score: 10, picture: "urldafoto"},
+      {name: "segundapessoa", score: 30, picture: "urldafoto"},
+      {name: "terceirapessoa", score: 20, picture: "urldafoto"},
+      {name: "quartapessoa", score: 25, picture: "urldafoto"},
+      {name: "quintapessoa", score: 50, picture: "urldafoto"}
+    ]
     return (
       <div>
-        Xablau
+        <ul>
+          { ranking
+            .sort((a, b) => b.score - a.score)
+            .map((player, index) => (
+          <li key={player.name} >
+            <div data-testid="player-picture-${index}">{ player.picture }</div>
+            <div data-testid="player-name-${index}">{ player.name }</div>
+            <div data-testid="player-score-${index}">{ player.score }</div>
+          </li>
+          ))}
+        </ul>
+        <button data-testid="btn-go-home">
+          <Link to="/">Voltar ao Início</Link>
+        </button>
       </div>
     );
   }
