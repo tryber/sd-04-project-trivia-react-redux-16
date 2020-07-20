478cde78fd69a938f9259650172651c0a747a085
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-07-20 18:17:01 -0300

    CC fix 2

--- src/components/question.js
+++ src/components/question.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { updatePlayer, updateScore } from '../redux/action';
+import { updateScore } from '../redux/action';
 
 class Question extends Component {
   constructor(props) {
