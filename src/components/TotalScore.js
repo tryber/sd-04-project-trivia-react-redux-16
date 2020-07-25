8cb66318a734848a875fa48b300a6a7ec330b286
Kaecio <kaecio@yahoo.com.br>
2020-07-24 21:13:35 -0300

    Estilizando o projeto

--- src/components/TotalScore.js
+++ src/components/TotalScore.js
@@ -1,22 +1,23 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './TotalScore.css';
 
 class TotalScore extends Component {
   render() {
     const { score, assertions } = this.props;
     return (
-      <div>
-        <h1 data-testid="feedback-total-question">
+      <div className="totalScore">
+        <h1 data-testid="feedback-total-question" className="totalScore-h1">
           Você acertou
           {` ${assertions} `}
           questões!
         </h1>
-        <div data-testid="feedback-total-score">
+        <h2 data-testid="feedback-total-score" className="totalScore-score">
           Um total de
           {` ${score} `}
           pontos
-        </div>
+        </h2>
       </div>
     );
   }
